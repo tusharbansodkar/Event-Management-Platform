a52962229c8105d099ed686911500060f7697bee
tushar <tusharbansodkar@gmail.com>
2025-02-10 10:35:50 +0530

    created working backend

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  const token = req.header("Authorization");
+  if (!token) return res.status(401).json({ message: "Access Denied" });
+
+  try {
+    const verified = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = authMiddleware;
